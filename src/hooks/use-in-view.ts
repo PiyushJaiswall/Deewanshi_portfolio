66153e197e8f0e966f8bffd52e4a0613065b8000
Piyush jaiswal <92733674+PiyushJaiswall@users.noreply.github.com>
2026-07-25 23:20:39 +0530

    Update use-in-view.ts

--- src/hooks/use-in-view.ts
+++ src/hooks/use-in-view.ts
@@ -3,9 +3,12 @@
 import { useRef } from "react";
 import { useInView as useFramerInView } from "framer-motion";
 
-export function useInView(margin: string = "0px 0px -100px 0px") {
+export function useInView() {
   const ref = useRef(null);
-  const isInView = useFramerInView(ref, { once: true, margin });
+  const isInView = useFramerInView(ref, {
+    once: true,
+    margin: "0px 0px -100px 0px",
+  });
 
   return { ref, isInView };
 }
